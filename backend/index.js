ad214f339c2126d9e39e62f8830518427a77777d
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-08-19 12:43:59 +0900

    .

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const cors = require("cors")
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+mongoose
+    .connect(process.env.MONGO_DB)
+    .then(() => console.log("MongoDB 연결 성공"))
+    .catch((err) => console.log("연결 실패", err))
+
+const bucketRoutes = require("./routes/bucketRoutes")
+app.use("/api/buckets", bucketRoutes)
+
+app.get("/", (req, res) => {
+    res.send("Hello Express")
+})
+
+app.listen(PORT, () => {
+    console.log("Server is Running!")
+})
