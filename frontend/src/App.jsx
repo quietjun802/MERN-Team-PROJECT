69df9714cba3b921c4b14aa31e3599160db58049
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-08-22 11:05:55 +0900

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,13 @@
 import { useState } from 'react'
-import Header from '../component/Header'
-import Input from '../component/Input'
-import Item from '../component/Item'
-import List from '../component/List'
 import './App.css'
+import BucketEditor from './components/BucketEditor'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 
-    <>
-      <Header />
-      <Input />
-      <Item />
-      <List />
-    </>
+      <BucketEditor />
   )
 }
 
